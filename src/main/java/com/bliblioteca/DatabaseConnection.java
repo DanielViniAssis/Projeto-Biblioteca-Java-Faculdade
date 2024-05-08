96dff6494f257e16fedf958638db29e8a4b0179f
Dankuzco <dvs.daniel11@gmail.com>
2024-05-08 11:56:59 -0300

    Atualizado

--- src/main/java/com/bliblioteca/DatabaseConnection.java
+++ src/main/java/com/bliblioteca/DatabaseConnection.java
@@ -2,6 +2,7 @@ package com.bliblioteca;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DatabaseConnection {
     private static final String URL = "jdbc:postgresql://localhost:5432/biblioteca";
@@ -10,5 +11,30 @@ public class DatabaseConnection {
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+
+    public static void main(String[] args) {
+        try (Connection conn = DriverManager.getConnection(URL, USER, PASSWORD)) {
+            if (conn != null) {
+                System.out.println("Conexão com o banco de dados estabelecida.");
+            
+                try (Statement statement = conn.createStatement()) {
+                    String sql = "CREATE TABLE IF NOT EXISTS livros ("
+                            + "id INTEGER PRIMARY KEY,"
+                            + "titulo VARCHAR,"
+                            + "autor VARCHAR,"
+                            + "paginas INTEGER,"
+                            + "ano_de_lancamento DATE"
+                            + ")";
+                    statement.execute(sql);
+                    
+                    System.out.println("Tabela criada com sucesso.");
+                } catch (SQLException e) {
+                    System.out.println("Erro ao criar a tabela: " + e.getMessage());
+                }
+            }
+        } catch (SQLException e) {
+            System.out.println("Erro ao conectar ao banco de dados: " + e.getMessage());
+        }
     }
 }
